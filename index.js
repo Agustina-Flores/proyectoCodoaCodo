9c35634e10bb3817c8df43b9c238321d54dfa824
Agustina-Flores <agustina.flores.mail@gmail.com>
2023-01-03 01:27:31 -0300

    parte2 simular producto(dinamico) y rutas

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express()
 var hbs = require('hbs');
 
+const productos = require('./data/food.json');
+
 hbs.registerPartials(__dirname + '/views/partials');//directorio hacia los parciales
 const port = 3000
 
@@ -12,9 +14,15 @@ app.use(express.static('public')); //indica carpeta public para considerar archi
 //renderiza el archivo index, que se encuentra en la carpeta views
 app.get('/', (req,res) =>{
 
+    // objeto con una propiedad llamada products, con un array de objetos
+    //console.log(productos);
+
     //indica nombre del archivo hbs,objeto
    res.render('index',{
-    titulo:"Mi pagina web"
+    titulo:"Mi pagina web",
+    productos: productos.products //tomo todos los productos del json
+
+     
    })
 })
 
@@ -35,6 +43,13 @@ app.get('/book', (req,res) =>{
    })
 })
 
+app.get('/comprar', (req,res) =>{
+
+    //indica nombre del archivo hbs,objeto
+   res.render('comprar',{
+    titulo:"Como Comprar"
+   })
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
